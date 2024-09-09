10b4b3bc38de497263fc56ac9c854232d7ab46ba
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-09 20:55:02 +0300

    working with application properties

--- src/main/java/com/dmtr/Springapp/controller/MyRestController.java
+++ src/main/java/com/dmtr/Springapp/controller/MyRestController.java
@@ -1,13 +1,26 @@
 package com.dmtr.Springapp.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
 
+    @Value("${first.text}")
+    private String hello;
+    @Value("${my.car}")
+    private String nameOfCar;
+    @Value("${my.city}")
+    private String nameOfCity;
+
+    @GetMapping("/carcity")
+    public String infoOfMyCarAndCity() {
+        return "My car = " + nameOfCar + ".\n"
+                + "my city = " + nameOfCity + ".";
+    }
     @GetMapping("/")
     public String helloWorld() {
-        return "Hello World";
+        return hello;
     }
 }
