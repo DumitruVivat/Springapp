d43e16918303f8d42e28dc35e02389da244a00d5
Dumitru Vivat <kaibaman94@gmail.com>
2024-09-09 17:40:24 +0300

    add controller

---
+++ src/main/java/com/dmtr/Springapp/controller/MyRestController.java
@@ -0,0 +1,13 @@
+package com.dmtr.Springapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MyRestController {
+
+    @GetMapping("/")
+    public String helloWorld() {
+        return "Hello World!";
+    }
+}
